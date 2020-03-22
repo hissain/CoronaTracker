ef9f0c2e93f8c9cf3823ee0d07a07e03d40a506a
Sazzad Hissain Khan <hissain.khan@gmail.com>
2020-03-22 15:34:39 +0600

    Adding server app on Spring Boot
    Signed-off-by: Sazzad Hissain Khan <hissain.khan@gmail.com>

---
+++ CoronaTrackerServer/src/main/java/com/coronatracker/bd/CoronaTrackerServer/CoronaTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.coronatracker.bd.CoronaTrackerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerServerApplication.class, args);
+	}
+
+}
---
+++ CoronaTrackerServer/src/test/java/com/coronatracker/bd/CoronaTrackerServer/CoronaTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronatracker.bd.CoronaTrackerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
