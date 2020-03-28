7715d1058b4e494642308c36abe6dea9f92ec2cf
Sazzad Hissain Khan <hissain.khan@gmail.com>
2020-03-28 22:01:27 +0600

    Implementing iOS Main Registration screen
    Signed-off-by: Sazzad Hissain Khan <hissain.khan@gmail.com>

--- iOS/CoronaTracker/CoronaTracker/Scenes/RegistrationViewController.swift
+++ iOS/CoronaTracker/CoronaTracker/Scenes/RegistrationViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.navigationController?.title = "Complete Registration"
     }
-
-
 }
 
