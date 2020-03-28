c7dafc85637a77f854de0c711fe26322d82d5ff5
Sazzad Hissain Khan <hissain.khan@gmail.com>
2020-03-28 12:13:16 +0600

    Android registation form
    Signed-off-by: Sazzad Hissain Khan <hissain.khan@gmail.com>

---
+++ Android/app/src/main/java/bd/coronatracker/com/coronatracker/RegistrationActivity.kt
@@ -0,0 +1,14 @@
+package bd.coronatracker.com.coronatracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle("Complete Registation")
+        setContentView(R.layout.activity_registration)
+    }
+}
